30bd17738c673ab79f52bd2cef1dfc7c8682ba05
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-11-01 16:28:30 +0900

    2. 변수, 상수

---
+++ Chapter02/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        int count;
+        count = 10;
+        count = 20;
+        System.out.println(count);
+//        count = 11.11;
+        double svg = 11.1;
+        String name = "minho";
+        System.out.println(name);
+
+        int i;
+        i = 10;
+        i = 5;
+        final int J;  // 상수는 대문자를 사용하는게 좋음
+        J = 10;
+//        J = 5; -> 불가
+
+        double circleArea; // 변수 : 두번째 단어 맨앞글자 대문자
+        final int OIL_PRICE; // 상수 : 대문자로 하고 단어사이에 _넣기
+
+    }
+}
+
+// 변수 : 값(데이터)이 변할 수 있는 값
+// 상수 : 수식에서 변하지 않는 값 -> final
