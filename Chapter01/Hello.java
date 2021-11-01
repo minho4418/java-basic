a2205579a26f25450948f7ae79e6cd91a7cfb72f
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-11-01 16:19:18 +0900

    1. 출력, 주석

---
+++ Chapter01/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 글자 출력
+        System.out.println(1);
+        System.out.println("1");
+       // System.out.println("Hello World");
+
+    }
+}
+
+/* 주석 */
