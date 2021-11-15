c1c0e3821f1e9bd23df53fabab5b7a8cd1dab6a5
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-11-15 22:14:44 +0900

    10. abstract, super

---
+++ Chapter10/super/Truck.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class Truck extends Car2{
+    public Truck(){
+        super("소방차"); // 부모 객체를 나타내는 키워드
+        System.out.println("Truck의 기본 생성자 입니다.");
+    }
+}
---
+++ Chapter10/super/TruckExam.java
@@ -0,0 +1,9 @@
+package javaStudy;
+
+public class TruckExam {
+    public static void main(String[] args){
+        Truck t = new Truck();
+
+       // Car2 c = new Car2();
+    }
+}
